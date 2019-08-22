00c164b01bf44ad19f12e916a746ded32796664f
HakonArnarS <43438456+HakonArnarS@users.noreply.github.com>
2019-08-22 14:33:18 +0000

    vista gögn af framenda í skjali á bakenda

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fs = require('fs'); 
 
 const app = express();
 app.use(cors());
@@ -8,12 +9,17 @@ app.use(
   bodyParser.json()
 );
 
-// app.use(() => {
-//   console.log('App use is running');
-// });
+
 
 app.post('/', (req, res) => {
   console.log(req.body);
+// add a line to a lyric file, using appendFile
+  fs.appendFile("./bloggdata", JSON.stringify(req.body), (err) => {
+      if (err) throw err;
+      console.log('The data was updated!');
+    });
+  
+  
 });
 app.get('/', (req, res) => {
   res.send('Muna að nota formið!!');
